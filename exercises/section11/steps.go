c72eddce8f639f5a6eaf31e1ffb395e8673da51b
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2021-09-13 21:54:16 +0100

    Add naive solution

--- exercises/section11/steps.go
+++ exercises/section11/steps.go
@@ -5,13 +5,38 @@ import (
 	"strings"
 )
 
-func executeStep(steps int) []string {
+func ExecuteStep(steps int) []string {
 	result := make([]string, 0, steps)
 	for i := 1; i <= steps; i++ {
 		s := strings.Repeat("#", i)
 		space := strings.Repeat(" ", steps-i)
-		step := fmt.Sprintf("%s%s\n", s, space)
+		step := fmt.Sprintf("%s%s", s, space)
 		result = append(result, step)
 	}
+	PrintRows(result)
 	return result
+}
+
+func ExecuteStepNaive(steps int) []string {
+	result := make([]string, 0, steps)
+	for row := 0; row < steps; row++ {
+		current := make([]string, 0, steps)
+		for column := 0; column < steps; column++ {
+			if column <= row {
+				current = append(current, "#")
+			} else {
+				current = append(current, " ")
+			}
+		}
+		cRow := strings.Join(current, "")
+		result = append(result, cRow)
+	}
+	PrintRows(result)
+	return result
+}
+
+func PrintRows(rows []string) {
+	for _, cRow := range rows {
+		fmt.Println(cRow)
+	}
 }
--- exercises/section11/steps_test.go
+++ exercises/section11/steps_test.go
@@ -1,8 +1,10 @@
-package section11
+package section11_test
 
 import (
 	"reflect"
 	"testing"
+
+	. "github.com/stevedesilva/dsa-golang.git/exercises/section11"
 )
 
 func Test_executeStep(t *testing.T) {
@@ -22,29 +24,74 @@ func Test_executeStep(t *testing.T) {
 		{
 			name: "1",
 			args: args{step: 1},
-			want: []string{"#\n"},
+			want: []string{"#"},
 		},
 		{
 			name: "2",
 			args: args{step: 2},
-			want: []string{"# \n", "##\n"},
+			want: []string{"# ", "##"},
 		},
 		{
 			name: "3",
 			args: args{step: 3},
-			want: []string{"#  \n", "## \n", "###\n"},
+			want: []string{"#  ", "## ", "###"},
 		},
 		{
 			name: "4",
 			args: args{step: 4},
-			want: []string{"#   \n", "##  \n", "### \n", "####\n"},
+			want: []string{"#   ", "##  ", "### ", "####"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := executeStep(tt.args.step); !reflect.DeepEqual(got, tt.want) {
+			if got := ExecuteStep(tt.args.step); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("executeStep() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+}
+
+func Test_executeStepNaive(t *testing.T) {
+	type args struct {
+		step int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "0",
+			args: args{step: 0},
+			want: []string{},
+		},
+		{
+			name: "1",
+			args: args{step: 1},
+			want: []string{"#"},
+		},
+		{
+			name: "2",
+			args: args{step: 2},
+			want: []string{"# ", "##"},
+		},
+		{
+			name: "3",
+			args: args{step: 3},
+			want: []string{"#  ", "## ", "###"},
+		},
+		{
+			name: "4",
+			args: args{step: 4},
+			want: []string{"#   ", "##  ", "### ", "####"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ExecuteStepNaive(tt.args.step); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("executeStep() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
 }
