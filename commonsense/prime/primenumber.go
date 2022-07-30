141c2215f6b9ed8884422f73b7f9ad9a23ff4f65
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-07-30 23:36:00 +0100

    Prime number

---
+++ commonsense/prime/primenumber.go
@@ -0,0 +1,13 @@
+package prime
+
+func IsPrime(num int) bool {
+	if num < 2 {
+		return false
+	}
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
---
+++ commonsense/prime/primenumber_test.go
@@ -0,0 +1,59 @@
+package prime
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsPrime(t *testing.T) {
+
+	data := []struct {
+		name     string
+		input    int
+		expected bool
+	}{
+		{
+			name:     "0",
+			input:    0,
+			expected: false,
+		},
+		{
+			name:     "1",
+			input:    0,
+			expected: false,
+		},
+
+		{
+			name:     "2",
+			input:    2,
+			expected: true,
+		},
+		{
+			name:     "3",
+			input:    3,
+			expected: true,
+		},
+		{
+			name:     "4",
+			input:    4,
+			expected: false,
+		},
+		{
+			name:     "5",
+			input:    5,
+			expected: true,
+		},
+		{
+			name:     "6",
+			input:    6,
+			expected: false,
+		},
+	}
+	for _, d := range data {
+
+		res := IsPrime(d.input)
+		assert.Equal(t, d.expected, res, d.name)
+	}
+
+}
