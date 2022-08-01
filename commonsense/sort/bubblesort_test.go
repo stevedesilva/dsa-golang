838aab0d553d1ef3c5088859f011af4231ed289f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-01 22:56:35 +0100

    bubble sort init

--- commonsense/sort/bubblesort_test.go
+++ commonsense/sort/bubblesort_test.go
@@ -14,35 +14,33 @@ func TestBubbleSort_Sort(t *testing.T) {
 		want []int
 	}{
 		{
-			name: "already sorted 1,2,3,4,5",
-			data: []int{1, 2, 3, 4, 5},
-			want: []int{1, 2, 3, 4, 5},
-		},
-		{
 			name: "sorted 1",
 			data: []int{1},
 			want: []int{1},
 		},
 		{
-			name: "sorted 1,2",
-			data: []int{2, 1},
-			want: []int{1, 2},
+			name: "already sorted 1,2,3,4,5",
+			data: []int{1, 2, 3, 4, 5},
+			want: []int{1, 2, 3, 4, 5},
 		},
-		{
-			name: "sorted 1,2,3",
-			data: []int{3, 2, 1},
-			want: []int{1, 2, 1},
-		},
+		//{
+		//	name: "sorted 1,2",
+		//	data: []int{2, 1},
+		//	want: []int{1, 2},
+		//},
+		//{
+		//	name: "sorted 1,2,3",
+		//	data: []int{3, 2, 1},
+		//	want: []int{1, 2, 1},
+		//},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			b := &BubbleSort{
 				data: tt.data,
 			}
-
-			want := []int{1, 2, 3, 4, 5}
 			b.Sort()
-			assert.Equal(t, want, b.data)
+			assert.Equal(t, tt.want, b.data)
 		})
 	}
 }
