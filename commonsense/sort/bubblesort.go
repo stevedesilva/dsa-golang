c6e38653b22d5678dafca42d734bb60d0945457f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-03 07:45:48 +0100

    bubble sort

--- commonsense/sort/bubblesort.go
+++ commonsense/sort/bubblesort.go
@@ -4,12 +4,31 @@ type BubbleSort struct {
 	data []int
 }
 
+//func (b *BubbleSort) Sort() {
+//	//swapped := f
+//	for i := 0; i < len(b.data); i++ {
+//		for j := i + 1; j < len(b.data); j++ {
+//			if b.data[i] > b.data[j] {
+//				b.data[j], b.data[i] = b.data[i], b.data[j]
+//			}
+//		}
+//	}
+//}
+
 func (b *BubbleSort) Sort() {
-	for i := 0; i < len(b.data); i++ {
-		for j := i + 1; j < len(b.data); j++ {
-			if b.data[i] > b.data[j] {
-				b.data[j], b.data[i] = b.data[i], b.data[j]
+	notSwapped := false
+	start := 0
+	end := len(b.data)
+	for i := start; i < end; i++ {
+		if !notSwapped {
+			return
+		}
+		for j := start + 1; j < end; j++ {
+			if b.data[j-1] > b.data[j] {
+				b.data[j], b.data[j-1] = b.data[j-1], b.data[j]
+				notSwapped = true
 			}
 		}
+		end--
 	}
 }
