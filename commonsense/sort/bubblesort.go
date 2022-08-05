7bd83994577c514ee7189c32263a23494e290d44
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-05 23:43:24 +0100

    bubble sort

--- commonsense/sort/bubblesort.go
+++ commonsense/sort/bubblesort.go
@@ -25,3 +25,23 @@ func (b *BubbleSort) Sort() {
 		}
 	}
 }
+
+func (b *BubbleSort) SortAlt() {
+	swapOccurred := false
+	end := len(b.data)
+	for i := 0; i < end; {
+		fmt.Println("i : ", i)
+		for j := i + 1; j < end; j++ {
+			if b.data[j-1] > b.data[j] {
+				b.data[j], b.data[j-1] = b.data[j-1], b.data[j]
+				swapOccurred = true
+			}
+		}
+		end--
+		fmt.Println("end: ", end)
+		fmt.Println("continue: ", i < end)
+		if !swapOccurred {
+			return
+		}
+	}
+}
