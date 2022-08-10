bbd132b4af9d70843712937108f65b2c5ef869cc
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-10 07:17:51 +0100

    Adding selection sort

--- commonsense/sort/selectionsort.go
+++ commonsense/sort/selectionsort.go
@@ -5,5 +5,15 @@ type SelectionSort struct {
 }
 
 func (s *SelectionSort) Sort() {
-
+	for i := 0; i < len(s.data); i++ {
+		lowestIdx := i
+		for j := i + 1; j < len(s.data); j++ {
+			if s.data[j] < s.data[lowestIdx] {
+				lowestIdx = j
+			}
+		}
+		if s.data[lowestIdx] < s.data[i] {
+			s.data[lowestIdx], s.data[i] = s.data[i], s.data[lowestIdx]
+		}
+	}
 }
