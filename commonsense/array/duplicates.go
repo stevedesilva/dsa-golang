65bc76b162f14cc147a1eea58bcd61b8caf072aa
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-13 09:49:33 +0100

    n squared solution

--- commonsense/array/duplicates.go
+++ commonsense/array/duplicates.go
@@ -1,15 +1,11 @@
 package array
 
 func HasDuplicates(array []int) bool {
-	foundValues := make([]bool, len(array))
 	for i := 0; i < len(array); i++ {
-		for j := i; j < len(array); j++ {
-			if foundValues[array[j]] {
+		for j := 0; j < len(array); j++ {
+			if i != j && array[i] == array[j] {
 				return true
-			} else {
-				foundValues[array[j]] = true
 			}
-
 		}
 	}
 	return false
