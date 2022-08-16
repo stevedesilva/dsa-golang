31867e34e707ad9a45cbc998bbfb3084c3e08a57
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-16 21:17:54 +0100

    initial commit for insertion sort

--- commonsense/sort/insertionsort.go
+++ commonsense/sort/insertionsort.go
@@ -1,5 +1,27 @@
 package sort
 
 func InsertionSort(array []int) []int {
+	// 1 select n + 1
+	// 2 take select out of array
+	// 3 compare n + 1 with left
+	// 4 if value less than selected number then swap - else move to next value in array
+	for i := 0; i < len(array); i++ {
+		value := array[i]
+		leastPos := i
+		prev := i - 1
+		for j := prev; j >= 0; j-- {
+			flag := array[j] > value
+			if flag {
+				array[leastPos] = array[j]
+				leastPos = j
+			} else {
+				break
+			}
+		}
+		if i != leastPos {
+			array[leastPos] = value
+		}
+
+	}
 	return array
 }
