64843000434bacccf70cebb32dc2c0f5aabaff67
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-27 08:09:03 +0100

    initial correct mean number check

--- commonsense/array/mean_number.go
+++ commonsense/array/mean_number.go
@@ -1,6 +1,23 @@
 package array
 
-func FindMeanForEvenNumbers(array []int) float64 {
+import "errors"
 
-	return 0
+var (
+	ErrNotFound = errors.New("no even numbers found")
+)
+
+func FindMeanForEvenNumbers(array []int) (float64, error) {
+	var sum float64 = 0
+	var count float64 = 0
+	for i := 0; i < len(array); i++ {
+		if array[i]%2 == 0 {
+			count++
+			sum = sum + float64(array[i])
+		}
+	}
+	if count < 1 {
+		return 0, ErrNotFound
+	}
+	res := sum / count
+	return res, nil
 }
--- commonsense/array/mean_number_test.go
+++ commonsense/array/mean_number_test.go
@@ -1,31 +1,55 @@
-package array
+package array_test
 
-import "testing"
+import (
+	"testing"
+
+	. "github.com/stevedesilva/dsa-golang.git/commonsense/array"
+)
 
 func TestFindMeanNumber(t *testing.T) {
 	type args struct {
 		array []int
 	}
+	type want struct {
+		res float64
+		err error
+	}
+
 	tests := []struct {
 		name string
 		args args
-		want int
+		want want
 	}{
 		{
 			name: "1",
 			args: args{array: []int{1}},
-			want: 1,
+			want: want{1, ErrNotFound},
 		},
 		{
 			name: "10,10",
-			args: args{array: []int{1}},
-			want: 10,
+			args: args{array: []int{10, 10}},
+			want: want{10, ErrNotFound},
+		},
+		{
+			name: "5,10",
+			args: args{array: []int{5, 10}},
+			want: want{10, ErrNotFound},
+		},
+		{
+			name: "8,8,8",
+			args: args{array: []int{8, 8, 8}},
+			want: want{8, ErrNotFound},
+		},
+		{
+			name: "6,8,20,12",
+			args: args{array: []int{6, 8, 20, 12}},
+			want: want{11.5, ErrNotFound},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := FindMeanForEvenNumbers(tt.args.array); got != tt.want {
-				t.Errorf("FindMeanForEvenNumbers() = %v, want %v", got, tt.want)
+			if got, err := FindMeanForEvenNumbers(tt.args.array); got != tt.want.res && err != tt.want.err {
+				t.Errorf("FindMeanForEvenNumbers() = %v, want result %v with error %v ", got, tt.want.res, tt.want.err)
 			}
 		})
 	}
