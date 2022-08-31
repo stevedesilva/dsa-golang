72d4aa28436d697d14dc238427a16f23a5ac7936
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-31 21:56:46 +0100

    Adding init func for avg celsuis reader

---
+++ commonsense/array/celsuis_reader.go
@@ -0,0 +1,11 @@
+package array
+
+func AverageCelsuisReader(fahrenheitReadings []int) float64 {
+	celsuisNumbers := make([]int, 0, len(fahrenheitReadings))
+	// convert fahrenheit to celsuis
+	_ = celsuisNumbers
+	// return average celsuis
+
+	return 0
+
+}
