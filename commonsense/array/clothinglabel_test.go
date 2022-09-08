ec0f081376e24dfc4d9d5eac39e751614e5a2f60
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-08 07:18:24 +0100

    Adding  test clothing list

--- commonsense/array/clothinglabel_test.go
+++ commonsense/array/clothinglabel_test.go
@@ -6,21 +6,42 @@ import (
 )
 
 func Test_markInventory(t *testing.T) {
-	type args struct {
-		items []string
-	}
+
 	tests := []struct {
 		name    string
-		args    args
+		args    []string
 		want    []string
-		wantErr bool
+		wantErr error
 	}{
-		{},
+		{
+			name:    "Purple Shirt",
+			args:    []string{"Purple Shirt"},
+			want:    []string{"Purple Shirt Size: 1,Purple Shirt Size: 2,Purple Shirt Size: 3,Purple Shirt Size: 4,Purple Shirt Size: 5"},
+			wantErr: nil,
+		},
+		{
+			name:    "Green Shirt",
+			args:    []string{"Green Shirt"},
+			want:    []string{"Green Shirt Size: 1,Green Shirt Size: 2,Green Shirt Size: 3,Green Shirt Size: 4,Green Shirt Size: 5"},
+			wantErr: nil,
+		},
+		{
+			name:    "Red Shirt",
+			args:    []string{"Red Shirt"},
+			want:    []string{"Red Shirt-Red Shirt Size: 1,Red Shirt Size: 2,Red Shirt Size: 3,Red Shirt Size: 4,Red Shirt Size: 5"},
+			wantErr: nil,
+		},
+		{
+			name:    "Pink Shirt,Red Shirt-Pink",
+			args:    []string{"Pink Shirt,Red Shirt-Pink"},
+			want:    []string{"Pink Shirt,Red Shirt-Pink Shirt Size: 1,Pink Shirt Size: 2,Pink Shirt Size: 3,Pink Shirt Size: 4,Pink Shirt Size: 5,Red Shirt Size: 1,Red Shirt Size: 2,Red Shirt Size: 3,Red Shirt Size: 4,Red Shirt Size: 5"},
+			wantErr: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := markInventory(tt.args.items)
-			if (err != nil) != tt.wantErr {
+			got, err := markInventory(tt.args)
+			if err != tt.wantErr {
 				t.Errorf("markInventory() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
