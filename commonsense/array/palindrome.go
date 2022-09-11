be3ba8d90bab05b2bd0478d576b6e11870fd889e
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-11 21:36:57 +0100

    added impl

---
+++ commonsense/array/palindrome.go
@@ -0,0 +1,5 @@
+package array
+
+func IsPalindrome(word string) bool {
+	return true
+}
---
+++ commonsense/array/palindrome_test.go
@@ -0,0 +1,54 @@
+package array
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T) {
+	tests := []struct {
+		name string
+		word string
+		want bool
+	}{
+		{
+			name: "a",
+			word: "a",
+			want: true,
+		},
+		{
+			name: "ADa",
+			word: "ADa",
+			want: true,
+		},
+		{
+			name: "KayAk",
+			word: "KayAk",
+			want: true,
+		},
+		{
+			name: "kayak",
+			word: "kayak",
+			want: true,
+		},
+		{
+			name: "Mom",
+			word: "Mom",
+			want: true,
+		},
+		{
+			name: "NoMatch",
+			word: "NoMatch",
+			want: false,
+		},
+		{
+			name: "Rampage",
+			word: "Rampage",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsPalindrome(tt.word); got != tt.want {
+				t.Errorf("IsPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
