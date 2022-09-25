8732d6902351618088849d9ac35613a899ffb8ea
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-25 09:10:16 +0100

    adding test for password hacker

--- commonsense/array/hacker.go
+++ commonsense/array/hacker.go
@@ -1,7 +1,14 @@
 package array
 
 // https://www.geeksforgeeks.org/print-all-combinations-of-given-length/
-func PasswordCracker(number int) []string {
-	res := make([]string, 0)
-	return res
+func PasswordCracker(set []rune, desiredLen int) []string {
+	i := len(set) * desiredLen
+	accumulator := make([]string, 0, i)
+	passwordCrackerRec(set, len(set), "", desiredLen, accumulator)
+	// convert acc
+	return accumulator
+}
+
+func passwordCrackerRec(set []rune, setLen int, prefix string, desiredLen int, accumulator []string) {
+
 }
--- commonsense/array/hacker_test.go
+++ commonsense/array/hacker_test.go
@@ -8,36 +8,48 @@ import (
 )
 
 func TestPasswordCracker(t *testing.T) {
-
+	type args struct {
+		charSet    []rune
+		desiredLen int
+	}
 	tests := []struct {
-		name   string
-		number int
-		want   []string
+		name string
+		args args
+		want []string
 	}{
 		{
-			name:   "empty",
-			number: 0,
-			want:   []string{},
+			name: "empty",
+			args: args{},
+			want: []string{},
 		},
 		{
-			name:   "single combi",
-			number: 1,
-			want:   []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"},
+			name: "single combination",
+			args: args{
+				charSet:    []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'},
+				desiredLen: 1,
+			},
+			want: []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"},
 		},
 		{
-			name:   "a b - 2",
-			number: 1,
-			want:   []string{"aa", "ab", "ba", "bb"},
+			name: "a b - len 2",
+			args: args{
+				charSet:    []rune{'a', 'b'},
+				desiredLen: 2,
+			},
+			want: []string{"aa", "ab", "ba", "bb"},
 		},
 		{
-			name:   "a b - 3",
-			number: 1,
-			want:   []string{"aaa", "aab", "aba", "abb", "baa", "bab", "bba", "bbb"},
+			name: "a b - 3",
+			args: args{
+				charSet:    []rune{'a', 'b'},
+				desiredLen: 3,
+			},
+			want: []string{"aaa", "aab", "aba", "abb", "baa", "bab", "bba", "bbb"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := array.PasswordCracker(tt.number); !reflect.DeepEqual(got, tt.want) {
+			if got := array.PasswordCracker(tt.args.charSet, tt.args.desiredLen); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("PasswordCracker() = %v, want %v", got, tt.want)
 			}
 		})
