d6d6b9e58e790c0e2f01abc3309efb0e605ecf71
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-29 21:47:41 +0100

    attempt impl for merged array

--- commonsense/array/merge.go
+++ commonsense/array/merge.go
@@ -1,5 +1,19 @@
 package array
 
-func mergeTwoArrays(a, b []int) (res []int) {
+func mergeTwoArrays(a, b []int) []int {
+	res := make([]int, 0, len(a)+len(b))
+	for i, j := 0, 0; i < len(a) || j < len(b); {
+
+		i2 := a[i]
+		i3 := b[j]
+		if i2 <= i3 {
+			res = append(res, a[i])
+			i++
+		} else {
+			res = append(res, b[j])
+			j++
+		}
+
+	}
 	return res
 }
