01b442a48c21a69d4adb4402d90538338383e2f6
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-29 23:22:57 +0100

    impl for merged array

--- commonsense/array/merge.go
+++ commonsense/array/merge.go
@@ -2,14 +2,25 @@ package array
 
 func mergeTwoArrays(a, b []int) []int {
 	res := make([]int, 0, len(a)+len(b))
+	//if len(a) <= 0 && len(b) > 0 {
+	//	return b
+	//}
+	//if len(b) <= 0 && len(a) > 0 {
+	//	return a
+	//}
 	for i, j := 0, 0; i < len(a) || j < len(b); {
-
-		i2 := a[i]
-		i3 := b[j]
-		if i2 <= i3 {
+		if i < len(a) && j < len(b) {
+			if a[i] <= b[j] {
+				res = append(res, a[i])
+				i++
+			} else {
+				res = append(res, b[j])
+				j++
+			}
+		} else if i < len(a) && j >= len(b) {
 			res = append(res, a[i])
 			i++
-		} else {
+		} else if j < len(b) && i >= len(a) {
 			res = append(res, b[j])
 			j++
 		}
