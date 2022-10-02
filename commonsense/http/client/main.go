74b9ca141ed3d0eda746cb218575f3c05e872988
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-10-02 20:41:25 +0100

    add server and client example

--- commonsense/http/client/main.go
+++ commonsense/http/client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
@@ -15,6 +16,7 @@ func main() {
 		mux := http.NewServeMux()
 		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("server: %s /\n", r.Method)
+			fmt.Fprintf(w, `{"message": "hello!"}`)
 		})
 		server := http.Server{
 			Addr:    fmt.Sprintf(":%d", serverPort),
@@ -29,11 +31,24 @@ func main() {
 
 	time.Sleep(100 * time.Millisecond)
 	requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
-	res, err := http.Get(requestURL)
+	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s \n", err)
+		os.Exit(1)
+	}
+
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Printf("error making http request: %s /\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("client: got response!\n")
 	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("client: could not read response body: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("client: response body: %s\n", resBody)
 }
