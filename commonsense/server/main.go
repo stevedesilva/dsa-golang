8b1fcad3207da949016fc68d3cf885612aad4879
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-10-02 10:24:10 +0100

    add server and client example

--- commonsense/server/main.go
+++ commonsense/server/main.go
@@ -13,9 +13,16 @@ func check(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Health check</h1>")
 }
 
+func callPalindrome(w http.ResponseWriter, r *http.Request) {
+
+	//array.IsPalindrome()
+	fmt.Fprintf(w, "<h1>The</h1>")
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/health_check", check)
+	http.HandleFunc("/function/palindrome", callPalindrome)
 	fmt.Println("Server starting...")
 	http.ListenAndServe(":8080", nil)
 }
