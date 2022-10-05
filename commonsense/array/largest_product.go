e6a25809fb4539c94978f5f8947d37be99b66f1f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-10-05 23:02:44 +0100

    largest product  impl

--- commonsense/array/largest_product.go
+++ commonsense/array/largest_product.go
@@ -10,5 +10,16 @@ func findLargestProduct(array []int) (int, error) {
 	if len(array) < 3 {
 		return 0, ErrMinimumInputNotMet
 	}
-	return 0, nil
+	largestProduct := array[0] * array[1] * array[2]
+	for i := 0; i < len(array); i++ {
+		for j := i + 1; j < len(array); j++ {
+			for k := j + 1; k < len(array); k++ {
+				curr := array[i] * array[j] * array[k]
+				if largestProduct < curr {
+					largestProduct = curr
+				}
+			}
+		}
+	}
+	return largestProduct, nil
 }
--- commonsense/array/largest_product_test.go
+++ commonsense/array/largest_product_test.go
@@ -20,6 +20,38 @@ func Test_findLargestProduct(t *testing.T) {
 			want:    0,
 			wantErr: ErrMinimumInputNotMet,
 		},
+		{
+			name: "3 numbers only",
+			args: args{
+				array: []int{1, 2, 3},
+			},
+			want:    6,
+			wantErr: nil,
+		},
+		{
+			name: "4 numbers only",
+			args: args{
+				array: []int{1, 2, 3, 4},
+			},
+			want:    24,
+			wantErr: nil,
+		},
+		{
+			name: "5 numbers only",
+			args: args{
+				array: []int{1, 2, 3, 4, 5},
+			},
+			want:    60,
+			wantErr: nil,
+		},
+		{
+			name: "5 numbers - first biggest",
+			args: args{
+				array: []int{5, 5, 3, 4, 5},
+			},
+			want:    125,
+			wantErr: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
