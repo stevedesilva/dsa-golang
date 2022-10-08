02a9b0e94f3d5f5dfae628222102a03d69a8daa3
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-10-08 22:45:31 +0100

    Adding impl for ludn algorithm

--- commonsense/array/ludn_algorithm.go
+++ commonsense/array/ludn_algorithm.go
@@ -1,9 +1,42 @@
 package array
 
-func IsCreditCardValidAsNumber(number int) bool {
-	return false
+import "strconv"
+
+func IsCreditCardValidAsNumber(creditCardNumber int) bool {
+	return IsCreditCardValidAsString(strconv.Itoa(creditCardNumber))
 }
 
-func IsCreditCardValidAsString(number string) bool {
-	return false
+func IsCreditCardValidAsString(creditCardNumber string) bool {
+	// double every number from right to left
+	workingNum := []rune(creditCardNumber)
+
+	flip := false
+	for i := len(workingNum) - 1; i >= 0; i-- {
+		curr := asciiToInt(workingNum[i])
+
+		if flip {
+			// if number is > 9 then split and add numbers together
+			curr *= 2
+			if curr > 9 {
+				digits := []rune(strconv.Itoa(int(curr)))
+				curr = asciiToInt(digits[0]) + asciiToInt(digits[1])
+			}
+			workingNum[i] = curr
+		} else {
+			workingNum[i] = curr
+		}
+		flip = !flip
+	}
+
+	// sum number
+	var sum rune
+	for _, v := range workingNum {
+		sum += v
+	}
+	// mod by 10 return true if number is 0
+	return (sum % 10) == 0
+}
+
+func asciiToInt(asciiValue rune) rune {
+	return asciiValue - '0'
 }
