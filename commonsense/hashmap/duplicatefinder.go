65ae9e462b34368c65ae5a2a58bc65889e2c69d9
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-10-13 23:37:37 +0100

    Duplicate finder

---
+++ commonsense/hashmap/duplicatefinder.go
@@ -0,0 +1,34 @@
+package hashmap
+
+import "errors"
+
+var ErrMinimumInputRequired = errors.New("minimum input is required")
+var ErrNoDuplicates = errors.New("no duplicates found in input data")
+
+type Duplicate interface {
+	FirstDuplicate() (*string, error)
+}
+
+func NewData() Duplicate {
+	return &data{
+		array: []string{},
+	}
+}
+
+type data struct {
+	array []string
+}
+
+func (d *data) FirstDuplicate() (*string, error) {
+	if len(d.array) < 2 {
+		return nil, ErrMinimumInputRequired
+	}
+	values := make(map[string]bool, len(d.array))
+	for _, v := range d.array {
+		if values[v] {
+			return &v, nil
+		}
+		values[v] = true
+	}
+	return nil, ErrNoDuplicates
+}
---
+++ commonsense/hashmap/duplicatefinder_test.go
@@ -0,0 +1,68 @@
+package hashmap
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_data_FirstDuplicate(t *testing.T) {
+	type fields struct {
+		array []string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    *string
+		wantErr error
+	}{
+		{
+			name:    "minimum not met",
+			fields:  fields{array: []string{}},
+			want:    nil,
+			wantErr: ErrMinimumInputRequired,
+		},
+		{
+			name:    "minimum not met with single value",
+			fields:  fields{array: []string{"a"}},
+			want:    nil,
+			wantErr: ErrMinimumInputRequired,
+		},
+		{
+			name:    "minimum not met with single value",
+			fields:  fields{array: []string{"a", "a"}},
+			want:    stringPtr("a"),
+			wantErr: nil,
+		},
+		{
+			name:    "find duplicate not met with single value",
+			fields:  fields{array: []string{"a", "b", "c", "a", "b", "d"}},
+			want:    stringPtr("a"),
+			wantErr: nil,
+		},
+		{
+			name:    "find duplicate not met with single value",
+			fields:  fields{array: []string{"ab", "bb", "cb", "ab", "bb", "db"}},
+			want:    stringPtr("ab"),
+			wantErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &data{
+				array: tt.fields.array,
+			}
+			got, err := d.FirstDuplicate()
+			if err != tt.wantErr {
+				t.Errorf("FirstDuplicate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FirstDuplicate() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func stringPtr(in string) *string {
+	return &in
+}
