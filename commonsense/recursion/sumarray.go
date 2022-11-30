b65cb2f7602f957e663f1f2a4c7680c7056f0b99
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-11-30 01:00:59 +0000

    add tests for sum array

---
+++ commonsense/recursion/sumarray.go
@@ -0,0 +1,5 @@
+package recursion
+
+func sum(array []int) int {
+	return 0
+}
---
+++ commonsense/recursion/sumarray_test.go
@@ -0,0 +1,47 @@
+package recursion
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_sum(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		array []int
+		want  int
+	}{
+		{
+			name:  "1",
+			array: []int{1},
+			want:  1,
+		},
+		{
+			name:  "1 - 2",
+			array: []int{1, 2},
+			want:  3,
+		},
+		{
+			name:  "1 - 3",
+			array: []int{1, 2, 3},
+			want:  15,
+		},
+		{
+			name:  "1 - 4",
+			array: []int{1, 2, 3, 4},
+			want:  15,
+		},
+		{
+			name:  "1 - 5",
+			array: []int{1, 2, 3, 4, 5},
+			want:  15,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, sum(tt.array), "sum(%v)", tt.array)
+		})
+	}
+}
