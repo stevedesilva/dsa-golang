ac1f3de6812fddd654ccc96d3e6890e652e8dbea
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-12-01 08:26:14 +0000

    And alternative recursive sum  array

--- commonsense/recursion/sumarray.go
+++ commonsense/recursion/sumarray.go
@@ -1,12 +1,27 @@
 package recursion
 
-func sumArrayStartingAtIndexZero(array []int) int {
+func SumArrayStartingAtZeroIndex(array []int) int {
 	return sumArrayStartingAtIndexZeroRec(array, 0)
 }
 
 func sumArrayStartingAtIndexZeroRec(array []int, total int) int {
+	firstElement := array[0]
 	if len(array) == 1 {
-		return array[0]
+		return firstElement
 	}
-	return array[0] + sumArrayStartingAtIndexZeroRec(array[1:], total)
+	sliceExcludingFirstElement := array[1:]
+	return firstElement + sumArrayStartingAtIndexZeroRec(sliceExcludingFirstElement, total)
+}
+
+func SumArrayStartingAtIndexEnd(array []int) int {
+	return sumArrayStartingAtIndexEndRec(array, 0)
+}
+
+func sumArrayStartingAtIndexEndRec(array []int, total int) int {
+	lastElement := array[len(array)-1]
+	if len(array) == 1 {
+		return lastElement
+	}
+	sliceExcludingLastElement := array[:len(array)-1]
+	return lastElement + sumArrayStartingAtIndexEndRec(sliceExcludingLastElement, total)
 }
--- commonsense/recursion/sumarray_test.go
+++ commonsense/recursion/sumarray_test.go
@@ -6,8 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_sum(t *testing.T) {
-
+func Test_SumArrayStartingAtZeroIndex(t *testing.T) {
 	tests := []struct {
 		name  string
 		array []int
@@ -41,7 +40,48 @@ func Test_sum(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, sumArrayStartingAtIndexZero(tt.array), "sumArrayStartingAtIndexZero(%v)", tt.array)
+			assert.Equalf(t, tt.want, SumArrayStartingAtZeroIndex(tt.array), "SumArrayStartingAtZeroIndex(%v)", tt.array)
+		})
+	}
+}
+
+func Test_SumArrayStartingAtIndexEnd(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		array []int
+		want  int
+	}{
+		{
+			name:  "1",
+			array: []int{1},
+			want:  1,
+		},
+		{
+			name:  "1 - 2",
+			array: []int{1, 2},
+			want:  3,
+		},
+
+		{
+			name:  "1 - 3",
+			array: []int{1, 2, 3},
+			want:  6,
+		},
+		{
+			name:  "1 - 4",
+			array: []int{1, 2, 3, 4},
+			want:  10,
+		},
+		{
+			name:  "1 - 5",
+			array: []int{1, 2, 3, 4, 5},
+			want:  15,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, SumArrayStartingAtIndexEnd(tt.array), "SumArrayStartingAtIndexEnd(%v)", tt.array)
 		})
 	}
 }
