80c2b73158583c211f6639455fe97f95163a1c63
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-12-10 23:49:19 +0000

    steps impl and test

--- commonsense/recursion/staircase.go
+++ commonsense/recursion/staircase.go
@@ -1,5 +1,22 @@
 package recursion
 
+func CountNumberOfSteps(number int) int {
+	return countNumberOfSteps(number)
+}
+
 func countNumberOfSteps(number int) int {
-	return 0
+	if number <= 0 {
+		return 0
+	}
+	if number == 1 {
+		return 1
+	}
+	if number == 2 {
+		return 2
+	}
+	if number == 3 {
+		return 4
+	}
+
+	return countNumberOfSteps(number-3) + countNumberOfSteps(number-2) + countNumberOfSteps(number-1)
 }
--- commonsense/recursion/staircase_test.go
+++ commonsense/recursion/staircase_test.go
@@ -13,6 +13,12 @@ func Test_countNumberOfSteps(t *testing.T) {
 		number int
 		want   int
 	}{
+		// 1 = 1
+		// 2 = 11, 2
+		// 3 = 111, 12, 21, 3
+		// 4 = 1111, 112, 121, 211, 22, 31, 13
+		// 5 = 11111, 1112, 1121, 1211, 2111, 221, 212, 122, 311, 131, 113, 32, 23
+		// 6 = 111111, 11112, 11121, 11211, 12111, 21111, 2211,2121,2112,222,231,213, 3111,1311,1131,1113, 321,312, 33
 		{
 			name:   "step 1",
 			number: 1,
@@ -33,10 +39,20 @@ func Test_countNumberOfSteps(t *testing.T) {
 			number: 4,
 			want:   7,
 		},
+		{
+			name:   "step 5",
+			number: 5,
+			want:   13,
+		},
+		{
+			name:   "step 5",
+			number: 6,
+			want:   19,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, countNumberOfSteps(tt.number), "countNumberOfSteps(%v)", tt.number)
+			assert.Equalf(t, tt.want, CountNumberOfSteps(tt.number), "CountNumberOfSteps(%v)", tt.number)
 		})
 	}
 }
