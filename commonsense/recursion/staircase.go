ee9d711ac4dfb02e4aab32a863b7b532ae3a1e86
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-12-11 22:34:44 +0000

    steps impl and test

--- commonsense/recursion/staircase.go
+++ commonsense/recursion/staircase.go
@@ -4,45 +4,32 @@ import "fmt"
 
 func CountNumberOfSteps(number int) int {
 	fmt.Println(">", number)
-	res := countNumberOfSteps(number)
+	res := countNumberOfPaths(number)
 	fmt.Println("<", res)
 	return res
 }
 
-//func countNumberOfSteps(number int) int {
-//
-//	if number <= 0 {
-//		return 0
-//	}
-//	if number == 1 {
-//		return 1
-//	}
-//	if number == 2 {
-//		return 2
-//	}
-//	if number == 3 {
-//		return 4
-//	}
-//
-//	fmt.Println(":", number)
-//	return countNumberOfSteps(number-1) + countNumberOfSteps(number-2) + countNumberOfSteps(number-3)
-//}
-
-func countNumberOfSteps(number int) int {
-
-	if number <= 0 {
+func countNumberOfPaths(steps int) int {
+	// hardcode base values
+	if steps <= 0 {
 		return 0
 	}
-	if number == 1 {
+	if steps == 1 {
 		return 1
 	}
-	if number == 2 {
+	if steps == 2 {
 		return 2
 	}
-	if number == 3 {
+	if steps == 3 {
 		return 4
 	}
+	// 7 = 6r + 5r + 4r  = 24 + 13 + 7 = 44
+	// 6 = 5r + 4r + 3(4) = 13 + 7 + 4 = 24
+	// 5 = 4r + 3(4) + 2(2) = 7 + 4 + 2 = 13
+	// 4 = 3(4) + 2(2) + 1(1) = 7
+	// 3 = 4
+	// 2 = 2
+	// 1 = 1
 
-	fmt.Println(":", number)
-	return countNumberOfSteps(number - 1)
+	return countNumberOfPaths(steps-1) + countNumberOfPaths(steps-2) + countNumberOfPaths(steps-3)
 }
