bcf4c31a19defd9ca27bb8fa95c35e9fa95b0d30
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-01-06 22:31:30 +0000

    add  impl for find max number

--- commonsense/recursion/dynamic_programming/maxnumber.go
+++ commonsense/recursion/dynamic_programming/maxnumber.go
@@ -6,8 +6,17 @@ func FindMaxNumber(numbers []int) (int, error) {
 	if len(numbers) < 1 {
 		return 0, errors.New("input minimum not met")
 	}
+	return findMaxNumber(numbers), nil
+}
+
+func findMaxNumber(numbers []int) int {
 	if len(numbers) == 1 {
-		return numbers[0], nil
+		return numbers[0]
 	}
-	return 0, nil
+	max := findMaxNumber(numbers[1:])
+	if numbers[0] > max {
+		return numbers[0]
+	} else {
+		return max
+	}
 }
