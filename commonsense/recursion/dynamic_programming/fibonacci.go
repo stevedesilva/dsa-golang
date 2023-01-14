d90ae6a50f6971dea4347613de86120cd17e9ed2
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-01-14 19:30:19 +0000

    add  fib impl

--- commonsense/recursion/dynamic_programming/fibonacci.go
+++ commonsense/recursion/dynamic_programming/fibonacci.go
@@ -1,5 +1,20 @@
 package dynamic_programming
 
+import "fmt"
+
 func Fibonacci(number int) int {
-	return 0
+	cache := make(map[int]int, 0)
+	return fibonacci(number, cache)
+}
+
+func fibonacci(number int, cache map[int]int) int {
+	if number <= 1 {
+		return number
+	}
+	if _, found := cache[number]; !found {
+		cache[number] = fibonacci(number-2, cache) + fibonacci(number-1, cache)
+		fmt.Println(cache[number])
+		return cache[number]
+	}
+	return cache[number]
 }
