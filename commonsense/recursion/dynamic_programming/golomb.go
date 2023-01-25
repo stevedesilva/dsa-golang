6c88768c12a0a746aa540148075fc9af923ce909
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-01-25 22:34:29 +0000

    Adding golomb refactor

--- commonsense/recursion/dynamic_programming/golomb.go
+++ commonsense/recursion/dynamic_programming/golomb.go
@@ -1,8 +1,20 @@
 package dynamic_programming
 
+import "fmt"
+
 func Golomb(number int) int {
+	cache := make(map[int]int)
+	return golomb(number, cache)
+}
+
+func golomb(number int, cache map[int]int) int {
 	if number == 1 {
 		return 1
 	}
-	return 1 + Golomb(number-Golomb(Golomb(number-1)))
+	if val, ok := cache[number]; ok {
+		fmt.Printf("cached number%d= %d \n", number, val)
+		return val
+	}
+	cache[number] = 1 + golomb(number-golomb(golomb(number-1, cache), cache), cache)
+	return cache[number]
 }
