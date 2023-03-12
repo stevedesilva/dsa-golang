57d7b735d9a27e0aa79a5bfbfa5086f194e15363
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-03-12 16:19:54 +0000

    add has missing numbers functionality

--- commonsense/sort/applications/missingnumber.go
+++ commonsense/sort/applications/missingnumber.go
@@ -1,6 +1,10 @@
 package applications
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/stevedesilva/dsa-golang.git/commonsense/sort"
+)
 
 var (
 	MinimumErr      = errors.New("minimum args not met")
@@ -11,11 +15,13 @@ func FindMissingNumber(numbers []int) (*int, error) {
 	if numbers == nil || len(numbers) < 1 {
 		return nil, MinimumErr
 	}
+	sort.NewQuickSort(numbers).Quicksort()
 	for i := 0; i < len(numbers); i++ {
 		if numbers[i] != i {
 			return IntPointer(i), nil
 		}
 	}
+
 	return nil, ItemNotFoundErr
 }
 
