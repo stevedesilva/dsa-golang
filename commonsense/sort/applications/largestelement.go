0006479ec5954914e1e0d8c72966bca87a182c00
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-03-15 06:49:48 +0000

    add n solution

--- commonsense/sort/applications/largestelement.go
+++ commonsense/sort/applications/largestelement.go
@@ -1,20 +1,39 @@
 package applications
 
-func LargestValueN(numbers []int) (*int, error) {
+import "errors"
 
-	return intPtr(0), nil
+var (
+	ErrMinimumValue = errors.New("minimum values of slice not supplied")
+)
+
+func LargestValueN(numbers []int) (*int, error) {
+	if len(numbers) < 1 {
+		return nil, ErrMinimumValue
+	}
+	maxValue := numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		if maxValue < numbers[i] {
+			maxValue = numbers[i]
+		}
+	}
+	return intPtr(maxValue), nil
 }
 
 func LargestValueNLogN(numbers []int) (*int, error) {
-
+	if len(numbers) < 1 {
+		return nil, ErrMinimumValue
+	}
 	return intPtr(0), nil
 }
 
 func LargestValueN2(numbers []int) (*int, error) {
-
+	if len(numbers) < 1 {
+		return nil, ErrMinimumValue
+	}
 	return intPtr(0), nil
 }
 
 func intPtr(n int) *int {
+
 	return &n
 }
