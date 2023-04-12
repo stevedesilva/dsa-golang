95ab0ca8c504247f3f8304fe0a09a5854ea921a3
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-04-12 07:36:35 +0100

    more regex

--- kata/v1/regexmatcher/regexmatcher.go
+++ kata/v1/regexmatcher/regexmatcher.go
@@ -6,13 +6,27 @@ import (
 )
 
 func main() {
-	matchRegex("test string peach peeach pch")
+	//matchRegex("test string peach peeach pch")
+	matchRegex2()
 }
 
 func matchRegex(input string) error {
 	r := regexp.MustCompile("p([a-z]+)ch")
-
 	fmt.Println("regexp:", r)
 	fmt.Println(r.ReplaceAllString(input, "<fruit>"))
+	fmt.Println("regexp:", r.FindString("regexp"))
 	return nil
+}
+func matchRegex2() {
+	rgx, _ := regexp.MatchString("p([a-z]+)ch", "test string peach peeach pch")
+	fmt.Println("matched(boolean)?:", rgx)
+	r := regexp.MustCompile("p([a-z]+)ch")
+
+	fmt.Println("MatchString(boolean)?:", r.MatchString("Test string peach peeach pinch punch pair."))
+	fmt.Println("FindString(string)?:", r.FindString("Test string peach peeach pinch punch pair."))
+	fmt.Println("FindStringIndex([]int)?:", r.FindStringIndex("peach peeach pinch punch pair."))
+	fmt.Println("FindStringSubmatch([]string)?:", r.FindStringSubmatch("pinch peach peeach pinch punch pair."))
+	fmt.Println("FindStringSubmatchIndex([]int)?:", r.FindStringSubmatchIndex("pinch peach peeach pinch punch pair."))
+	fmt.Println("FindAllString([]string)?:", r.FindAllString("pinch peach peeach pinch punch pair.", -1))
+	fmt.Println("limit FindAllString([]string)?:", r.FindAllString("pinch peach peeach pinch punch pair.", 2))
 }
