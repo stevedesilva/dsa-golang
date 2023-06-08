adee03e2eb53427bb0bc712a8dfa172f129c8d2d
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-06-08 22:26:24 +0100

    adding tests for treeNode

--- commonsense/tree/binarysearch/treenode_test.go
+++ commonsense/tree/binarysearch/treenode_test.go
@@ -1,1 +1,15 @@
 package binarysearch
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTreeNode(t *testing.T) {
+	left := TreeNode[int]{1, nil, nil}
+	right := TreeNode[int]{3, nil, nil}
+	root := TreeNode[int]{2, &left, &right}
+	assert.Equal(t, root.data, 2)
+	assert.Equal(t, root.right.data, 3)
+	assert.Equal(t, root.left.data, 1)
+}
