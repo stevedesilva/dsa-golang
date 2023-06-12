cf805ad275d697251b337c15d91e302d5f1d7fca
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-06-12 07:36:28 +0100

    add impl for search

--- commonsense/tree/binarysearch/treenode.go
+++ commonsense/tree/binarysearch/treenode.go
@@ -1,23 +1,23 @@
 package binarysearch
 
-type TreeNode[T comparable] struct {
+import "golang.org/x/exp/constraints"
+
+type TreeNode[T constraints.Ordered] struct {
 	data  T
 	left  *TreeNode[T]
 	right *TreeNode[T]
 }
 
-func (t *TreeNode[T]) Search(value T) bool {
+func (t *TreeNode[T]) Search(value interface{}) *TreeNode[T] {
 	return t.search(value, t)
 }
 
-func (t *TreeNode[T]) search(value T, node *TreeNode[T]) bool {
-	// base
-	if node == nil {
-		return false
+func (t *TreeNode[T]) search(value T, node *TreeNode[T]) *TreeNode[T] {
+	if node == nil || value == node.data {
+		return node
 	}
-	if value == node.data {
-		return true
+	if value <= node.data {
+		return t.search(value, node.left)
 	}
-
-	return t.search(value, node.left) || t.search(value, node.right)
+	return t.search(value, node.right)
 }
--- go.mod
+++ go.mod
@@ -5,13 +5,14 @@ go 1.19
 require (
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
+	golang.org/x/net v0.4.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
