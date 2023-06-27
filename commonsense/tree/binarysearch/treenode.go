ea0b2880770f7b17cda7679763c6cee0a5defe50
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-06-27 06:46:24 +0100

    add impl for search

--- commonsense/tree/binarysearch/treenode.go
+++ commonsense/tree/binarysearch/treenode.go
@@ -22,3 +22,23 @@ func (t *TreeNode[T]) search(value T, node *TreeNode[T]) *TreeNode[T] {
 		return t.search(value, node.right)
 	}
 }
+
+func (t *TreeNode[T]) Insert(value T) *TreeNode[T] {
+	return t.insert(value, t)
+}
+
+func (t *TreeNode[T]) insert(value T, node *TreeNode[T]) *TreeNode[T] {
+	if node == nil || node.data == value {
+		return node
+	} else if value < node.data {
+		if node.left == nil {
+			node.left = &TreeNode[T]{value, nil, nil}
+		}
+		return t.insert(value, node.left)
+	} else {
+		if node.right == nil {
+			node.right = &TreeNode[T]{value, nil, nil}
+		}
+		return t.insert(value, node.right)
+	}
+}
--- commonsense/tree/binarysearch/treenode_test.go
+++ commonsense/tree/binarysearch/treenode_test.go
@@ -28,3 +28,52 @@ func TestTreeNodeSearch(t *testing.T) {
 	assert.Equal(t, 7, root.Search(7).data)
 	assert.Nil(t, root.Search(19))
 }
+
+func TestTreeNodeInsertInOrder(t *testing.T) {
+	//      1
+	//     / \
+	//    2   nil
+	//    ..
+	//   /
+	//  7
+	root := TreeNode[int]{}
+	root.Insert(1)
+	root.Insert(2)
+	root.Insert(3)
+	root.Insert(4)
+	root.Insert(5)
+	root.Insert(6)
+	root.Insert(7)
+
+	assert.Equal(t, 1, root.Search(1).data)
+	assert.Equal(t, 2, root.Search(2).data)
+	assert.Equal(t, 3, root.Search(3).data)
+	assert.Equal(t, 4, root.Search(4).data)
+	assert.Equal(t, 5, root.Search(5).data)
+	assert.Equal(t, 6, root.Search(6).data)
+	assert.Equal(t, 7, root.Search(7).data)
+}
+
+func TestTreeNodeInsertRandom(t *testing.T) {
+	//      4
+	//     / \
+	//    3   5
+	//   / \  / \
+	//  1  2  6  7
+	root := TreeNode[int]{}
+	root.Insert(4)
+	root.Insert(5)
+	root.Insert(3)
+	root.Insert(6)
+	root.Insert(1)
+	root.Insert(2)
+	root.Insert(7)
+
+	assert.Equal(t, 1, root.Search(1).data)
+	assert.Equal(t, 2, root.Search(2).data)
+	assert.Equal(t, 3, root.Search(3).data)
+	assert.Equal(t, 4, root.Search(4).data)
+	assert.Equal(t, 5, root.Search(5).data)
+	assert.Equal(t, 6, root.Search(6).data)
+	assert.Equal(t, 7, root.Search(7).data)
+}
