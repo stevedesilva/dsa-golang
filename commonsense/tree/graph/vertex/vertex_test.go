ee92937f898908b28ba47e479a5a2f3309700273
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-01-28 14:53:44 +0000

    add test for dfs traversal

--- commonsense/tree/graph/vertex/vertex_test.go
+++ commonsense/tree/graph/vertex/vertex_test.go
@@ -1,6 +1,9 @@
 package vertex
 
 import (
+	"bufio"
+	"bytes"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,5 +28,39 @@ func TestVertx_DFS_Traverse(t *testing.T) {
 	v1.AddAdjacentVertex(v2)
 	v1.AddAdjacentVertex(v3)
 
+	// create a pipe and reader
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Error("Error creating pipe: ", err)
+	}
+	// save the original out stream
+	origStdout := os.Stdout
+
+	// redirect stdout to buf
+	os.Stdout = w
+
+	// capture the output in a separate goroutine because writing to the pipe is synchronous
+	outCh := make(chan string)
+	go func() {
+		// create a buffer to capture your output
+		var buf bytes.Buffer
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			buf.Write(scanner.Bytes())
+			buf.WriteByte('\n')
+		}
+		outCh <- buf.String()
+	}()
+
+	// do work then close writer
 	v1.DfsTraverse(v2)
+	_ = w.Close()
+
+	// reset the output back to orig std out after test
+	os.Stdout = origStdout
+	output := <-outCh
+
+	expected := "2\n1\n3\n"
+	assert.Equal(t, output, expected)
+
 }
