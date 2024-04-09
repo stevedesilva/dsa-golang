01616863b46e4e406ea996f3de3f810d3289a066
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-04-09 09:57:49 +0100

    add city tests

--- commonsense/tree/graph/dijkstra/city.go
+++ commonsense/tree/graph/dijkstra/city.go
@@ -4,3 +4,14 @@ type City struct {
 	Name   string
 	Routes map[string]int
 }
+
+func NewCity(name string) *City {
+	return &City{
+		Name:   name,
+		Routes: make(map[string]int),
+	}
+}
+
+func (c *City) AddRoute(name string, value int) {
+	c.Routes[name] = value
+}
---
+++ commonsense/tree/graph/dijkstra/dikstra.go
@@ -0,0 +1,5 @@
+package dijkstra
+
+func ShortestPath(start, destination City) []string {
+	return []string{"A", "B", "C"}
+}
