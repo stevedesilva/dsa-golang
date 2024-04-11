189abf00153499f2d52fe59a1ae940802e5a5da8
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-04-11 19:02:51 +0100

    start

--- commonsense/tree/graph/dijkstra/dikstra.go
+++ commonsense/tree/graph/dijkstra/dikstra.go
@@ -4,5 +4,14 @@ type Dijkstra struct {
 }
 
 func (d *Dijkstra) ShortestPath(start, destination *City) []string {
+	// setup - previousCity, visitedCities, unvisitedCities, cheapestPriceFromStartingCity
+	previousVisitedCity := make(map[string]string)
+	visitedCities := make(map[string]bool)
+	unvisitedCities := make([]string, 0, len(start.Routes))
+	currentCity := start
+	// for each route in starting
+	// calculate shortest
+
+	// reverse  previousVisitedCity and current
 	return []string{"A", "B", "C"}
 }
