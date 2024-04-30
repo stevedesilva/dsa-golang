b996d23ee6bf043c0f2fe5b7ad6dda98c8328219
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-04-30 08:03:02 +0100

    update shortest path algorithm

--- commonsense/tree/graph/dijkstra/dikstra.go
+++ commonsense/tree/graph/dijkstra/dikstra.go
@@ -3,15 +3,53 @@ package dijkstra
 type Dijkstra struct {
 }
 
-func (d *Dijkstra) ShortestPath(start, destination *City) []string {
-	// setup - previousCity, visitedCities, unvisitedCities, cheapestPriceFromStartingCity
-	previousVisitedCity := make(map[string]string)
+func (d *Dijkstra) ShortestPath(startCity, destinationCity *City) []string {
+	cheapestPriceFromStartingCity := make(map[string]int)
+	cheapestPreviousStopoverCity := make(map[string]string)
+
 	visitedCities := make(map[string]bool)
-	unvisitedCities := make([]string, 0, len(start.Routes))
-	currentCity := start
+	unvisitedCities := make([]City, 0, 10)
+	// initialize
+	cheapestPriceFromStartingCity[startCity.Name] = 0
+	currentCity := startCity
+
 	// for each route in starting
+	for currentCity != nil {
+		// process current city
+		visitedCities[currentCity.Name] = true
+		removeElement(currentCity.Name, unvisitedCities)
+
+		for adjacentCityName, adjacentCityCost := range currentCity.Routes {
+			if !visitedCities[adjacentCityName] {
+				unvisitedCities = append(unvisitedCities, adjacentCityName)
+			}
+			// cost from current city to adjacentCity
+			priceThroughStartingCity := cheapestPriceFromStartingCity[currentCity.Name] + adjacentCityCost
+			if existingCheapestPriceForCity, ok := cheapestPriceFromStartingCity[adjacentCityName]; !ok || priceThroughStartingCity < existingCheapestPriceForCity {
+				cheapestPriceFromStartingCity[adjacentCityName] = priceThroughStartingCity
+				cheapestPreviousStopoverCity[adjacentCityName] = currentCity.Name
+			}
+		}
+
+		// chose next city
+		var city *City
+		//for i,c := range unvisitedCities {
+		//	cu
+		//	if c.
+		//}
+		currentCity = city
+	}
 	// calculate shortest
 
-	// reverse  previousVisitedCity and current
+	// reverse  cheapestPreviousStopoverCity and current
 	return []string{"A", "B", "C"}
+}
+
+func removeElement(element string, slice []string) []string {
+	for i, v := range slice {
+		if v == element {
+			return append(slice[:i], slice[i+1:]...)
+		}
+	}
+	return slice
 }
