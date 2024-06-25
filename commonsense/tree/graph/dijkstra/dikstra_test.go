3d8b715d95d755e00db69c751a1ee70197d66898
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-06-25 22:31:16 +0100

    tests

--- commonsense/tree/graph/dijkstra/dikstra_test.go
+++ commonsense/tree/graph/dijkstra/dikstra_test.go
@@ -31,3 +31,29 @@ func TestShortestPath(t *testing.T) {
 	expected := []string{"Atlanta", "Denver", "Chicago", "El Paso"}
 	assert.Equal(t, expected, shortestPath)
 }
+
+func TestShortestPath2(t *testing.T) {
+
+	// Given setup city with Routes
+	atlanta := NewCity("Atlanta")
+	chicago := NewCity("Chicago")
+	boston := NewCity("Boston")
+	denver := NewCity("Denver")
+	elPaso := NewCity("El Paso")
+
+	atlanta.AddRoute(boston, 100)
+	atlanta.AddRoute(denver, 160)
+	boston.AddRoute(chicago, 120)
+	boston.AddRoute(denver, 180)
+	chicago.AddRoute(elPaso, 80)
+	denver.AddRoute(chicago, 40)
+	denver.AddRoute(elPaso, 140)
+	elPaso.AddRoute(boston, 100)
+
+	// When
+	d := Dijkstra{}
+	shortestPath := d.ShortestPath(atlanta, denver)
+	// Then
+	expected := []string{"Atlanta", "Denver"}
+	assert.Equal(t, expected, shortestPath)
+}
