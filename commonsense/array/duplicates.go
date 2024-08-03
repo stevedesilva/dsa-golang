21128eb365c1b33f7af0f4052120fc79c7f411de
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-08-03 09:10:28 +0100

    duplicate test

--- commonsense/array/duplicates.go
+++ commonsense/array/duplicates.go
@@ -12,12 +12,19 @@ func HasDuplicates(array []int) bool {
 }
 
 func FindDuplicates(array []string) bool {
-	for i := 0; i < len(array); i++ {
-		for j := 0; j < len(array); j++ {
-			if i != j && array[i] == array[j] {
-				return true
-			}
+	table := make(map[string]bool)
+	for _, v := range array {
+		if _, ok := table[v]; ok {
+			return true
 		}
+		table[v] = true
 	}
+	return false
+}
+
+func FindDuplicates2(array []string) bool {
+	// sort array
+	sort
+	// compare adjacent elements
 	return false
 }
