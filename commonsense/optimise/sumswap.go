895930c6613093e6bc3880b330b5c8766140cb4c
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-01-06 17:10:03 +0000

    sumswap impl

--- commonsense/optimise/sumswap.go
+++ commonsense/optimise/sumswap.go
@@ -1,5 +1,7 @@
 package optimise
 
+import "fmt"
+
 func SumSwap(a, b []int) (int, int) {
 	sumA, sumB := 0, 0
 	for _, v := range a {
@@ -23,6 +25,76 @@ func SumSwap(a, b []int) (int, int) {
 	return 0, 0
 }
 
-func SwapToMakeEqual(a1, a2 []int) []int {
-	return []int{0, 0}
+/*
+    public int[] swapToMakeEqual(int[] a1, int[] a2) {
+        validate(a1, a2);
+        // key
+        Map<Integer, Integer> a1Map = new HashMap<>();
+        // get a sum of a1 while storing it's values in hashmap
+        int a1Total = 0;
+        for (int index = 0; index < a1.length; index++) {
+            final int number = a1[index];
+            a1Total += number;
+            a1Map.put(number, index);
+        }
+
+        final int a2Total = Arrays.stream(a2).sum();
+
+        // calculate the difference between the two arrays
+        final int difference = (a1Total - a2Total) / 2;
+
+        for (int i=0; i < a2.length; i++) {
+            // check map for the numbers counterpart in the 1st array,
+            // which is calculated as the current number
+            // plus the amount it has to shift by:
+            final int num = a2[i];
+            final int numToFind = num + difference;
+            try {
+                if (a1Map.containsKey(numToFind)) {
+//                    return new int[]{a1[numToFind],i};
+                    return new int[]{a1Map.get(numToFind),i};
+                }
+            } catch (Exception e) {
+                // continue
+            }
+
+        }
+        // throw item not found exception
+        throw new IllegalArgumentException("no items found");
+    }
+*/
+
+func SwapToMakeEqual(a1, a2 []int) ([]int, error) {
+	// validate if a1 or a2 is empty
+	if len(a1) == 0 || len(a2) == 0 {
+		return nil, fmt.Errorf("empty array")
+	}
+	// get a sum of a1 while storing it's values in hashmap
+	sumA := 0
+	hashA := make(map[int]int)
+	for i, v := range a1 {
+		sumA += v
+		hashA[v] = i
+	}
+
+	// get a sum of a2
+	sumB := 0
+	for _, v := range a2 {
+		sumB += v
+	}
+
+	// calculate the difference
+	diff := (sumA - sumB) / 2
+
+	// check map for the numbers counterpart in the 1st array,
+	// which is calculated as the current number
+	// plus the amount it has to shift by:
+	for _, v := range a2 {
+		numToFind := diff + v
+		if _, ok := hashA[numToFind]; ok {
+			return []int{numToFind, v}, nil
+		}
+	}
+
+	return nil, fmt.Errorf("empty array")
 }
--- commonsense/optimise/sumswap_test.go
+++ commonsense/optimise/sumswap_test.go
@@ -37,7 +37,7 @@ func TestSwapToMakeEqual(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := SwapToMakeEqual(tt.args.a, tt.args.a)
+			got, _ := SwapToMakeEqual(tt.args.a, tt.args.a)
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Got %v, want %v", got, tt.want)
 			}
