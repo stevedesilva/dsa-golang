fcc6c5d7338af9562fa7fb5789ed7e76cc2bfc43
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-06-29 13:31:13 +0100

    add max number func

--- commonsense/optimise/maxnum.go
+++ commonsense/optimise/maxnum.go
@@ -6,9 +6,6 @@ func MaxNum(array []int) (int, error) {
 	if len(array) == 0 {
 		return 0, errors.New("Array cannot be empty")
 	}
-	if len(array) == 1 {
-		return array[0], nil
-	}
 	max := array[0]
 	for _, v := range array {
 		if v > max {
