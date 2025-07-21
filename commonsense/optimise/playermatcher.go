095b1ad1926474d7b0387cfb16a1034f32421bf6
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-07-21 07:49:13 +0100

    add anagram check function

---
+++ commonsense/optimise/playermatcher.go
@@ -0,0 +1,17 @@
+package optimise
+
+type PlayerMatcher struct {
+	firstName string
+	lastName  string
+	team      string
+}
+
+type PlayerResult struct {
+	firstName string
+	lastName  string
+}
+
+func IsPlayerMatch(playersA, playersB []PlayerMatcher) []PlayerResult {
+
+	return nil
+}
