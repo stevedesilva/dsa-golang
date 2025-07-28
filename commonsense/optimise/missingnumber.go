937e020c69e2414556abaa4b87701b296be7d8d9
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-07-28 21:48:01 +0100

    add init for missing numbers test

--- commonsense/optimise/missingnumber.go
+++ commonsense/optimise/missingnumber.go
@@ -3,8 +3,45 @@ package optimise
 import "errors"
 
 func Find(numbers []int) (int, error) {
-	// find missing number in a sorted array of integers
+	if len(numbers) == 0 {
+		return 0, errors.New("empty list")
+	}
+	numberSet := make(map[int]bool)
+	for _, n := range numbers {
+		numberSet[n] = true
+	}
+	for i := 0; i < len(numbers); i++ {
+		if !numberSet[i] {
+			return i, nil
+		}
+	}
 
 	// if number not found, return error
-	return 0, errors.New("number not found")
+	return 0, errors.New("no missing number found")
 }
+
+//func Find(numbers []int) (int, error) {
+//	if len(numbers) == 0 {
+//		return 0, errors.New("empty list")
+//	}
+//
+//	// Calculate the expected sum of numbers from 0 to n
+//	n := len(numbers)
+//	expectedSum := n * (n + 1) / 2
+//
+//	// Calculate the actual sum of the given numbers
+//	actualSum := 0
+//	for _, num := range numbers {
+//		actualSum += num
+//	}
+//
+//	// The missing number is the difference between the expected and actual sums
+//	missingNumber := expectedSum - actualSum
+//
+//	// Validate if the missing number is within the valid range
+//	if missingNumber < 0 || missingNumber > n {
+//		return 0, errors.New("no missing number found")
+//	}
+//
+//	return missingNumber, nil
+//}
--- commonsense/optimise/missingnumber_test.go
+++ commonsense/optimise/missingnumber_test.go
@@ -20,10 +20,35 @@ func TestFind(t *testing.T) {
 		{
 			name: "Test with missing number in the middle",
 			args: args{
-				numbers: []int{1, 2, 4, 5, 6},
+				numbers: []int{0, 1, 2, 4, 5, 6},
 			},
 			want:    3,
 			wantErr: assert.NoError,
+		},
+		{
+			name: "Test with missing number in the middle",
+			args: args{
+				numbers: []int{0, 1, 2, 3, 5, 6},
+			},
+			want:    4,
+			wantErr: assert.NoError,
+		},
+		// no missing number
+		{
+			name: "Test with no missing number",
+			args: args{
+				numbers: []int{0, 1, 2, 3, 4, 5, 6},
+			},
+			want:    0,
+			wantErr: assert.Error,
+		},
+		{
+			name: "Test with empty array",
+			args: args{
+				numbers: []int{},
+			},
+			want:    0,
+			wantErr: assert.Error,
 		},
 	}
 	for _, tt := range tests {
