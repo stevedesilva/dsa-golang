882d5288ce5e53929ecd2180369dd3b594dc1c32
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-09-21 08:22:30 +0100

    first attempt

---
+++ commonsense/optimise/bodytemperature.go
@@ -0,0 +1,10 @@
+package optimise
+
+/*
+Takes a [] of float64 representing body temperatures in Fahrenheit
+Sorts the temperatures into ascending order.
+return error if any temperature in the array is not in range 97.0 to 99.0 inclusive
+*/
+func SortBodyTemperatures(temp []float64) ([]float64, error) {
+	return nil, nil
+}
---
+++ commonsense/optimise/bodytemperature_test.go
@@ -0,0 +1,61 @@
+package optimise
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSortBodyTemperatures(t *testing.T) {
+	type args struct {
+		temp []float64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []float64
+		wantErr bool
+	}{
+		{
+			name:    "Test with valid temperatures 1 element",
+			args:    args{temp: []float64{97.5}},
+			want:    []float64{97.5},
+			wantErr: false,
+		},
+		{
+			name:    "Test with valid temperatures 2 elements",
+			args:    args{temp: []float64{99.1, 97.5}},
+			want:    []float64{97.5, 99.1},
+			wantErr: false,
+		},
+		{
+			name:    "Test with valid temperatures 3 elements",
+			args:    args{temp: []float64{98.6, 101.3, 97.5}},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "Test with invalid temperature below range",
+			args:    args{temp: []float64{96.9, 98.6, 97.5}},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "Test with invalid temperature above range",
+			args:    args{temp: []float64{97.5, 99.0, 99.1}},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SortBodyTemperatures(tt.args.temp)
+			if tt.wantErr {
+				assert.Error(t, err, "Expected an error but got none")
+			} else {
+				assert.NoError(t, err, "Did not expect an error but got one")
+				assert.Equalf(t, tt.want, got, "SortBodyTemperatures(%v)", tt.args.temp)
+			}
+		})
+	}
+}
